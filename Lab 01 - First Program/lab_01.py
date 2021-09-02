61dff15966aad049ddff6688f0b3c028fac1c225
Simon Lewiston <simon.lewiston@my.simpson.edu>
2021-09-02 15:26:10 -0500

    Lab 1

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,4 @@
+print("I think this class is going to be cool.")
+print("Zach said \"Pizza sounds good right now.\"")
+print("I grew up in Ogden, Iowa.")
+print("The Chicago Bears start their season next week!")
