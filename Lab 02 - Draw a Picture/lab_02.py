368a640bf741b0d8908a2dfbee256dea20b23b4f
Simon Lewiston <simon.lewiston@my.simpson.edu>
2021-09-09 15:53:19 -0500

    Work on Lab 2

---
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -0,0 +1,50 @@
+"""
+This program is for my Lab 2 project.
+"""
+
+# Import the "Arcade Library"
+import arcade
+
+# Open up a window.
+arcade.open_window(600, 600, "Lab 2 Boat")
+
+# Set the background color.
+arcade.set_background_color(arcade.color.SKY_BLUE)
+
+# Get ready to draw.
+arcade.start_render()
+
+# Drawing the water.
+arcade.draw_lrtb_rectangle_filled(0, 600, 250, 0, arcade.color.BLUE)
+
+# Drawing the base of the boat.
+arcade.draw_arc_filled(300, 280, 300, 200, arcade.color.BROWN, 180, 360)
+
+# Drawing the Mast of the boat.
+arcade.draw_rectangle_filled(350, 380, 20, 350, arcade.color.BROWN)
+
+# Drawing the house on the boat.
+arcade.draw_rectangle_filled(330, 300, 180, 40, arcade.color.WHITE)
+
+# Drawing the sail of the boat.
+arcade.draw_triangle_filled(340, 550, 340, 350, 100, 350, arcade.color.WHITE)
+
+# Drawing the broom of the boat.
+arcade.draw_rectangle_filled(225, 350, 245, 5, arcade.color.BROWN)
+
+# Drawing the windows on the boat.
+arcade.draw_circle_filled(370, 295, 10, arcade.color.LIGHT_BLUE)
+arcade.draw_circle_filled(340, 295, 10, arcade.color.LIGHT_BLUE)
+arcade.draw_circle_filled(310, 295, 10, arcade.color.LIGHT_BLUE)
+arcade.draw_circle_filled(280, 295, 10, arcade.color.LIGHT_BLUE)
+
+# Name of the boat.
+arcade.draw_text("Seas the Day",
+                 355, 310,
+                 arcade.color.REDWOOD, 7)
+
+# Finish drawing.
+arcade.finish_render()
+
+# Keep the window up until someone closes it.
+arcade.run()
