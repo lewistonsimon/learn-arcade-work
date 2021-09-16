e0d08244a52eec8efbefdb3a6bda1db186a3c3ba
Simon Lewiston <simon.lewiston@my.simpson.edu>
2021-09-16 15:51:12 -0500

    Lap 3

---
+++ Lab 03 - Draw Using Functions/lab_03.py
@@ -0,0 +1,77 @@
+import arcade
+
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+
+
+def draw_water():
+    arcade.draw_lrtb_rectangle_filled(0, 600, 250, 0, arcade.color.BLUE)
+
+
+def draw_sailboat():
+    # Sail
+    arcade.draw_triangle_filled(340, 550, 340, 350, 100, 350, arcade.color.WHITE)
+    arcade.draw_rectangle_filled(225, 350, 245, 5, arcade.color.BROWN)
+    arcade.draw_rectangle_filled(350, 380, 20, 350, arcade.color.BROWN)
+
+    # Base
+    arcade.draw_arc_filled(300, 280, 300, 200, arcade.color.BROWN, 180, 360)
+    arcade.draw_rectangle_filled(330, 300, 180, 40, arcade.color.WHITE)
+    arcade.draw_circle_filled(370, 295, 10, arcade.color.LIGHT_BLUE, num_segments=32)
+    arcade.draw_circle_filled(340, 295, 10, arcade.color.LIGHT_BLUE, num_segments=32)
+    arcade.draw_circle_filled(310, 295, 10, arcade.color.LIGHT_BLUE, num_segments=32)
+    arcade.draw_circle_filled(280, 295, 10, arcade.color.LIGHT_BLUE, num_segments=32)
+    arcade.draw_text("Seas the Day",
+                     355, 310,
+                     arcade.color.REDWOOD, 7)
+
+
+def draw_fish(x, y):
+    arcade.draw_triangle_filled(34 + x, 50 + y, 34 + x, 20 + y, 10 + x, 50 + y, arcade.color.ORANGE)
+    arcade.draw_ellipse_filled(40 + x, 50 + y, 35, 25, arcade.color.ORANGE)
+    arcade.draw_circle_filled(45 + x, 55 + y, 3, arcade.color.BLACK, num_segments=32)
+
+
+def draw_cloud(x, y):
+    arcade.draw_circle_filled(30 + x, 55 + y, 20, arcade.color.WHITE)
+    arcade.draw_circle_filled(15 + x, 40 + y, 20, arcade.color.WHITE)
+    arcade.draw_circle_filled(45 + x, 40 + y, 20, arcade.color.WHITE)
+    arcade.draw_ellipse_filled(30 + x, 40 + y, 55, 50, arcade.color.WHITE)
+
+
+def draw_birds(x, y):
+    arcade.draw_circle_filled(100 + x, 300 + y, 5, arcade.color.BLACK, num_segments=32)
+    arcade.draw_arc_outline(85 + x, 300 + y, 20, 15, arcade.color.BLACK, 0, 180, 5, 1)
+    arcade.draw_arc_outline(115 + x, 300 + y, 20, 15, arcade.color.BLACK, 0, 180, 5, 1)
+
+
+def main():
+    arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Lab 3 Boat")
+    arcade.set_background_color(arcade.color.SKY_BLUE)
+    arcade.start_render()
+
+    draw_water()
+    draw_sailboat()
+    draw_fish(200, 20)
+    draw_fish(150, 5)
+    draw_fish(15, 10)
+    draw_fish(60, 20)
+    draw_fish(200, 60)
+    draw_fish(100, 60)
+    draw_cloud(500, 520)
+    draw_cloud(10, 500)
+    draw_cloud(100, 490)
+    draw_cloud(70, 430)
+    draw_birds(350, 70)
+    draw_birds(400, 60)
+    draw_birds(450, 75)
+
+    # Finish drawing.
+    arcade.finish_render()
+
+    # Keep the window up until someone closes it.
+    arcade.run()
+
+
+# Call the main function to get the program started.
+main()
