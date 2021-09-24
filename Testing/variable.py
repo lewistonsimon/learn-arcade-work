c9e328d038b98ee56abddcb6fd599adf3bfa1663
lewistonsimon <simon.lewiston@my.simpson.edu>
2021-09-24 12:31:12 -0500

    Lab 4

--- Testing/variable.py
+++ Testing/variable.py
@@ -1,2 +1,37 @@
-answer = 42
-print(f"The answer is {answer}.")
+# answer = 42
+# print(f"The answer is {answer}.")
+
+# def average(x, y, z):
+#     my_average = (x + y + z) / 3
+#     return my_average
+#
+#
+# n1 = 8
+# n2 = 29
+# n3 = 78
+# new_average = average(n1, n2, n3)
+# print(new_average)
+
+# done = False
+# while not done:
+#     quit = input("Do you want to quit?")
+#     if quit == "yes":
+#         done == True
+#
+#     attack = input("Do you attack the dragon? ")
+#     if attack == "yes":
+#         print("Bad choice, you died.")
+#         done = True
+
+def average(x, y, z, w):
+    my_average = (x + y + z + w) / 4
+    return my_average
+
+
+n1 = 2
+n2 = 5
+n3 = 9
+n4 = 8
+new_average = average(n1, n2, n3, n4)
+print(new_average)
+
