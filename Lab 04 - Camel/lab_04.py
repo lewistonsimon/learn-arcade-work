6901e881e74724cacd3981e3cdfe0892f954a3f9
lewistonsimon <simon.lewiston@my.simpson.edu>
2021-09-27 22:00:20 -0500

    Lab 4

--- Lab 04 - Camel/lab_04.py
+++ Lab 04 - Camel/lab_04.py
@@ -2,7 +2,8 @@ import random
 
 
 def main():
-    print("Welcome to the desert!")
+    # Introduction
+    print("Welcome to Camel!")
     print("You have taken a camel from a local tribe to make your way across the desert.")
     print("The natives are chasing you to get their camel back.")
     print("Survive your trip and do not get caught.")
@@ -13,6 +14,7 @@ def main():
     natives_traveled = -20
     drinks_in_canteen = 3
 
+    # Choosing an Action.
     done = False
     while not done:
         print("A. Drink from your canteen.")
@@ -22,18 +24,27 @@ def main():
         print("E. Status check.")
         print("Q. Quit.")
         choice = input("What is your choice? ")
+
+        # Quit
         if choice.lower() == "q":
             done = True
             print("Have a nice day.")
+
+        # Status Check
         elif choice.lower() == "e":
             print("You have traveled", miles_traveled, "miles.")
             print("You have", drinks_in_canteen, "drinks left.")
             print("The natives are", (miles_traveled - natives_traveled), "miles behind you.")
+
+        # Stopping for the night.
         elif choice.lower() == "d":
             native_number_now = random.randrange(7, 15)
             camel_tiredness = 0
             natives_traveled += native_number_now
             print("You had a good night sleep.")
+            print("Your camel is happy.")
+
+        # Drinking from the canteen
         elif choice.lower() == "a":
             if drinks_in_canteen >= 1:
                 drinks_in_canteen -= 1
@@ -41,6 +52,8 @@ def main():
                 print("You have quenched your thirst. ")
             else:
                 print("You do not have any water left.")
+
+        # Ahead moderate speed.
         elif choice.lower() == "b":
             camel_distance = random.randrange(5, 13)
             miles_traveled += camel_distance
@@ -55,6 +68,8 @@ def main():
                 thirst = 0
                 camel_tiredness = 0
                 print("You found an oasis!")
+
+        # Ahead full speed.
         elif choice.lower() == "c":
             camel_distance = random.randrange(10, 21)
             miles_traveled += camel_distance
@@ -70,14 +85,18 @@ def main():
                 thirst = 0
                 camel_tiredness = 0
                 print("You found an oasis!")
-        else:
-            print("error please pick again.")
 
+        else:
+            print("error: please pick again.")
+
+        # Winning
         if not done:
             if miles_traveled >= 200:
+                print("You survived your journey across the desert!")
                 print("You Win!")
                 done = True
 
+        # Checking thirst.
         if not done:
             if thirst > 4 and thirst <= 6:
                 print("You are thirsty.")
@@ -85,6 +104,7 @@ def main():
                 print("You died of thirst.")
                 done = True
 
+        # Checking the camel.
         if not done:
             if camel_tiredness > 5 and camel_tiredness <= 8:
                 print("Your camel is getting tired.")
@@ -93,6 +113,7 @@ def main():
                 caught = (miles_traveled - natives_traveled)
                 natives_traveled += caught
 
+        # Natives chase
         if not done:
             if natives_traveled >= miles_traveled:
                 print("You have been caught by the natives.")
