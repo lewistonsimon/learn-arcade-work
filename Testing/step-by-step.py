7ea7e675db8f746b5512db2eb89bba962a44e22b
Simon Lewiston <simon.lewiston@my.simpson.edu>
2021-10-11 11:24:08 -0500

    Lab 6

--- Testing/step-by-step.py
+++ Testing/step-by-step.py
@@ -158,7 +158,209 @@
 #     zip_code: str = ""
 
 
-my_list = []
-user_input = input()
-my_list.append(user_input)
-print(my_list)
+# my_list = []
+# user_input = input()
+# my_list.append(user_input)
+# print(my_list)
+
+
+
+
+
+# class Cat:
+#     population
+#
+#     def __init__(self, name):
+#         self.name = name
+#         Cat.population +=
+#
+# def main():
+#     cat1 = Cat("Pat")
+#     cat2 = Cat("Pepper")
+#     cat3 = Cat("Pouncy")
+#
+#     print("The cat population is", Cat.population)
+#
+# main()
+
+
+
+# Chapter 17
+# Class Methods
+
+# class Dog:
+#     def __init__(self):
+#         self.age = 0
+#         self.name = ""
+#         self.weight = 0
+#
+#     # a function in a class is called a method
+#     def bark(self):
+#         if self.weight < 10:
+#             print("Yip! says", self.name)
+#         else:
+#             print("woof says", self.name)
+#
+# def main():
+#     my_dog = Dog()
+#     my_dog.name = "spot"
+#     my_dog.weight = 9
+#     my_dog.age = 3
+#     my_dog.bark()
+#
+#     my_other_dog = Dog()
+#     my_other_dog.name = "Fluffy"
+#     my_other_dog.weight = 20
+#     my_other_dog.age = 3
+#     my_other_dog.bark()
+#
+# main()
+
+# 17.1.1
+# References
+# class Person():
+#     def __init__(self):
+#         self.name = ""
+#         self.money = 0
+#
+#
+# def main(): # bob and nancy are just pointers
+#     bob = Person()
+#     bob.name = "Bob"
+#     bob.money = 100
+#
+#     nancy = Person()
+#     nancy.name = "Nancy"
+#
+#     print(bob.name, "has", bob.money, "dollars.")
+#     print(nancy.name, "has", nancy.money, "dollars.")
+#
+#
+# main()
+
+
+# class Person():
+#     def __init__(self):
+#         self.name = ""
+#         self.money = 0
+#
+#
+# def main(): # bob and nancy are just pointers
+#     bob = Person()
+#     bob.name = "Bob"
+#     bob.money = 100
+#
+#     nancy = bob
+#     nancy.name = "Nancy"
+#
+#     print(bob.name, "has", bob.money, "dollars.")
+#     print(nancy.name, "has", nancy.money, "dollars.")
+#
+#
+# main()
+
+
+# def give_money1(person):
+#     person.money += 100
+#
+#
+# class Person():
+#     def __init__(self):
+#         self.name = ""
+#         self.money = 0
+#
+#
+# def main():
+#     bob = Person()
+#     bob.name = "Bob"
+#     bob.money = 100
+#
+#     give_money1(bob)
+#     print(bob.money)
+#
+# main()
+
+# Wrong
+# class Dog():
+#     def __init__(self):
+#         self.age = 0
+#         self.name = ""
+#         self.weight = 0
+#
+#     def __init__(self): # it will ignore the first def __init__(self): and only do the second
+#         print("New dog!")
+#
+# # Correct:
+# class Dog():
+#     def __init__(self):
+#         self.age = 0
+#         self.name = ""
+#         self.weight = 0
+#         print("New dog!")
+
+
+# class Boat():
+#     def __init__(self):
+#         self.tonnage = 0
+#         self.name = ""
+#         self.is_docked = True
+#
+#     def dock(self):
+#         if self.is_docked:
+#             print("You are already docked.")
+#         else:
+#             self.is_docked = True
+#             print("Docking")
+#
+#     def undock(self):
+#         if not self.is_docked:
+#             print("You aren't docked.")
+#         else:
+#             self.is_docked = False
+#             print("Undocking")
+#
+# class Submarine(Boat): # a child class will get everything that the parent class
+#     def submerge(self):
+#         print("Submerge!")
+
+
+# class Person():
+#     def __init__(self):
+#         self.name = ""
+#
+# class Employee(Person):
+#     def __init__(self):
+#         # Call the parent/super class constructor first
+#         super().__init__()
+#
+#         # Now set up our variables
+#         self.job_title = ""
+#
+# class Customer(Person):
+#     def __init__(self):
+#         super().__init__()
+#         self.email = ""
+#
+# def main():
+#     john_smith = Person()
+#     john_smith.name = "John Smith"
+#
+#     jane_employee = Employee()
+#     jane_employee.name = "Jane Employee"
+#     jane_employee.job_title = "Web Developer"
+#
+#     bob_customer = Customer()
+#     bob_customer.name = "Bob Customer"
+#     bob_customer.email = "send_me@spam.com"
+#
+# main()
+
+
+"""
+
+Is-A
+Has-A
+
+
+
+"""
