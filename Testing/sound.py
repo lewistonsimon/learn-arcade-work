c3240af118dde201d0aeb6c27ba3c09e04a242b4
Simon Lewiston <simon.lewiston@my.simpson.edu>
2021-10-25 11:22:55 -0500

    lab 7

--- Testing/sound.py
+++ Testing/sound.py
@@ -1,9 +1,9 @@
-import arcade
-
-arcade.open_window(600, 600, "Sound Demo")
-laser_sound = arcade.load_sound(":resources:sounds/fall2.wav")
-arcade.play_sound(laser_sound)
-arcade.run()
+# import arcade
+#
+# arcade.open_window(600, 600, "Sound Demo")
+# laser_sound = arcade.load_sound(":resources:sounds/fall2.wav")
+# arcade.play_sound(laser_sound)
+# arcade.run()
 
 #
 # Sprites and Collisions
@@ -21,6 +21,14 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 
+class Coin(arcade.Sprite):
+    def update(self):
+        self.center_y -= 1
+        if self.top <= 0:
+            self.bottom = random.randrange(SCREEN_HEIGHT, SCREEN_HEIGHT * 1.2)
+            self.center_x = random.randrange(SCREEN_WIDTH)
+
+
 class MyGame(arcade.Window):
     """ Our custom Window Class"""
 
@@ -29,13 +37,59 @@ class MyGame(arcade.Window):
         # Call the parent class initializer
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprite Example")
 
+        self.player_list = None
+        self.coin_list = None
+
+        self.player_sprite = None
+        self.score = 0
+
+        self.set_mouse_visible(False)
+        arcade.set_background_color(arcade.color.AMAZON)
+
+    def setup(self):
+
+        self.player_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
+
+        self.score = 0
+
+        self.player_sprite = arcade.Sprite("character.png", SPRITE_SCALING_PLAYER)
+        self.player_sprite.center_x = 50
+        self.player_sprite.center_y = 50
+        self.player_list.append(self.player_sprite)
+
+        for i in range(COIN_COUNT):
+            coin = Coin("coin_01.png", SPRITE_SCALING_COIN)
+            coin.center_x = random.randrange(SCREEN_WIDTH)
+            coin.center_y = random.randrange(SCREEN_HEIGHT)
+            self.coin_list.append(coin)
+
     def on_draw(self):
         arcade.start_render()
+        self.player_list.draw()
+        self.coin_list.draw()
+
+        output = f"Score: {self.score}"
+        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 28)
+
+    def on_mouse_motion(self, x: float, y: float, dx: float, dy: float):
+        self.player_sprite.center_x = x
+        self.player_sprite.center_y = y
+
+    def on_update(self, delta_time):
+
+        self.coin_list.update()
+
+        coin_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
+        for coin in coin_hit_list:
+            coin.remove_from_sprite_lists()
+            self.score += 1
 
 
 def main():
     """ Main method """
     window = MyGame()
+    window.setup()
     arcade.run()
 
 
