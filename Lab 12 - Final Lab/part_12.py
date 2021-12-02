7f518d852c592f2d6f7f6579a648409fcdf79a07
lewistonsimon <simon.lewiston@my.simpson.edu>
2021-12-02 10:34:28 -0600

    lab 12

--- Lab 12 - Final Lab/part_12.py
+++ Lab 12 - Final Lab/part_12.py
@@ -27,12 +27,16 @@ class Item:
 
 
 def main():
+    # Introductin
+    print("Welcome")
 
     # Rooms
     room_list = []
+
+    # Room 0
     room = Room("You are standing outside on a farm. The corn is tall and green.\n"
                 "Clouds fill the sky and the noise of distant tractors fill your ears.\n"
-                "The wind ruffles the leaves in the trees and you see squirrels running.\n"
+                "The wind ruffles the leaves in the trees and you see squirrels running around the yard.\n"
                 "There is an old red barn to the east. The door is slide open.\n"
                 "Paint is beginning to chip off of it causing it to look rundown.\n"
                 "To the west there is a white ranch style house.\n"
@@ -40,6 +44,7 @@ def main():
                 13, 10, None, 1, None, None, None, None, None, None)
     room_list.append(room)
 
+    # Room 1
     room = Room("You're standing in an entry way. The light is dim and pictures of landscapes line the wall.\n"
                 "The tile floor is covered in dirt spots most likely leftover from work boots.\n"
                 "You are met with three different hallways to the north, south, and west.\n"
@@ -47,6 +52,7 @@ def main():
                 2, 0, 5, 8, None, None, None, None, None, None)
     room_list.append(room)
 
+    # Room 2
     room = Room("You entered a long hallway. There is no overhead lighting.\n"
                 "You can make out the outlines of clay statues that line the wall.\n"
                 "There is a door on the west side of the hall.\n"
@@ -55,6 +61,7 @@ def main():
                 None, 3, 1, 9, None, 12, None, None, None, None)
     room_list.append(room)
 
+    # Room 3
     room = Room("You are standing in the living room.\n"
                 "There is a large window that has a view of the corn field outside.\n"
                 "There is a TV, a couch, and multiple chairs.\n"
@@ -63,6 +70,7 @@ def main():
                 None, None, None, 2, None, None, None, None, None, None)
     room_list.append(room)
 
+    # Room 4
     room = Room("You are standing in the kitchen. Pots and pans line the counter of last night's dinner.\n"
                 "The fruit bowl is sitting on the table. Flies have begun to gather around the rotting fruit.\n"
                 "Spoons have been spilled and now cover the floor.\n"
@@ -70,6 +78,7 @@ def main():
                 None, None, None, 5, None, None, None, None, None, None)
     room_list.append(room)
 
+    # Room 5
     room = Room("You are in a hallway. Light is shining through the small window on the south wall.\n"
                 "The hard tile floor is clean and looks recently redone.\n"
                 "You can smell something like rotten eggs behind the east door.\n"
@@ -78,6 +87,7 @@ def main():
                 1, 4, None, 6, None, None, None, None, None, None)
     room_list.append(room)
 
+    # Room 6
     room = Room("You are in the guest bedroom.\n"
                 "There is a small twin bed and a dresser that is missing the bottom drawer.\n"
                 "A brown suitcase is sitting in the corner. Above the suitcase there is a cracked window.\n"
@@ -85,18 +95,21 @@ def main():
                 None, 5, None, None, None, None, None, None, None, None)
     room_list.append(room)
 
+    # Room 7
     room = Room("You are standing in the master bedroom. The king size bed is a mess.\n"
                 "Cloths are scattered throughout the room.\n"
                 "You can head north back into the hallway",
                 8, None, None, None, None, None, None, None, None, None)
     room_list.append(room)
 
+    # Room 8
     room = Room("You are standing in a hallway. 18th century paintings lines the walls. \n"
                 "There are doors to your north and south.\n"
                 "You can also head east down a hallway.",
                 9, 1, 7, None, None, None, None, None, None, None)
     room_list.append(room)
 
+    # Room 9
     room = Room("You are in the office. There is desk that is neatly organized.\n"
                 "There is a stapler and a copier on the desk.\n"
                 "Book shelves full of large text books line the walls.\n"
@@ -104,6 +117,7 @@ def main():
                 None, 2, 8, None, None, None, None, None, None, None)
     room_list.append(room)
 
+    # Room 10
     room = Room("You are standing in the barn. The air is still.\n"
                 "Dim lights in the rafter show dust sitting in the air.\n"
                 "Hay is lining the floor and the smell of animals fill the air.\n"
@@ -112,80 +126,98 @@ def main():
                 None, None, None, 0, 11, None, None, None, None, None)
     room_list.append(room)
 
+    # Room 11
     room = Room("You are in the old hay loft. The floor is filled with cracked boards.\n"
                 "There is some loose twine hanging from the rafters above you.\n"
                 "",
                 None, None, None, None, None, 10, None, None, None, None)
     room_list.append(room)
 
+    # Room 12
     room = Room("You are standing in the basement. It is cold and damp.\n"
                 "Cobwebs line the ceiling.",
                 None, None, None, None, 2, None, None, None, None, None)
     room_list.append(room)
 
-    room = Room("You have entered the corn field. It can be tricky to navigate through.\n"
-                "The yard is to your south. You can go deeper into the field by going north, northeast, or northwest.",
-                15, None, 0, None, None, None, 16, 14, None, None) # 13
+    # Room 13
+    room = Room("You have entered the corn field. You can see the blue sky above you.\n"
+                "The yard is to your south. You can go deeper into the field by going north, northeast, or northwest.\n"
+                "You can also walk the front fence line to the east or west.",
+                15, 23, 0, 24, None, None, 16, 14, None, None)
     room_list.append(room)
 
-    room = Room("You have entered deeper into the corn field.\n"
-                "You can go deeper into the field by going north, northeast, or northwest.",
-                17, None, None, None, None, None, None, None, 13, 24) # 14
+    # Room 14
+    room = Room("The corn is getting thick and everything looks the same.\n"
+                "You can continue by going north, southeast, or southwest.",
+                17, None, None, None, None, None, None, None, 13, 24)
     room_list.append(room)
 
-    room = Room("15 You have entered deeper into the corn field.\n"
-                "You can go deeper into the field by going north, northeast, or northwest.",
-                19, None, 13, None, None, None, None, 25, None, None)  # 15
+    # Room 15
+    room = Room("The corn is getting thick and everything looks the same.\n"
+                "You can continue by going north, south, northeast, or northwest.",
+                19, None, 13, None, None, None, 20, 25, None, None)
     room_list.append(room)
 
-    room = Room("16 You have entered deeper into the corn field.\n"
-                "You can go deeper into the field by going north, northeast, or northwest.",
-                20, None, 23, None, None, None, 21, None, 22, 13)  # 16
+    # Room 16
+    room = Room("The corn is getting thick and everything looks the same.\n"
+                "You can continue by going north, south, northeast, southeast, or southwest.",
+                20, None, 23, None, None, None, 21, None, 22, 13)
     room_list.append(room)
 
-    room = Room("17 You have entered deeper into the corn field.\n"
-                "You can go deeper into the field by going north, northeast, or northwest.",
-                25, None, 14, None, None, None, None, 18, None, None)  # 17
+    # Room 17
+    room = Room("The corn is getting thick and everything looks the same.\n"
+                "You can continue by going north, south, northeast, or northwest.",
+                25, None, 14, None, None, None, 19, 18, None, None)
     room_list.append(room)
 
-    room = Room("18 You have entered deeper into the corn field.\n"
-                "You can go deeper into the field by going north, northeast, or northwest.",
-                None, 25, 24, None, None, None, None, None, 17, None)  # 18
+    # Room 18
+    room = Room("You are standing in the far corner of the corn field.\n"
+                "You can continue by going east, south, or southeast.",
+                None, 25, 24, None, None, None, None, None, 17, None)
     room_list.append(room)
 
-    room = Room("19 You have entered deeper into the corn field.\n"
-                "You can go deeper into the field by going north, northeast, or northwest.",
-                None, None, 15, 25, None, None, None, None, 20, None)  # 19
+    # Room 19
+    room = Room("You are standing along the fence line at the opposite end of the property.\n"
+                "You can continue by going south, southeast, southwest, or west.",
+                None, None, 15, 25, None, None, None, None, 20, 17)
     room_list.append(room)
 
-    room = Room("20 You have entered deeper into the corn field.\n"
-                "You can go deeper into the field by going north, northeast, or northwest.",
-                None, 21, 16, None, None, None, None, 20, None, None)  # 20
+    # Room 20
+    room = Room("The corn is getting thick and everything looks the same.\n"
+                "You can continue by going east, south, northwest, or southwest.",
+                None, 21, 16, None, None, None, None, 20, None, 15)
     room_list.append(room)
 
-    room = Room("21 You have entered deeper into the corn field.\n"
-                "You can go deeper into the field by going north, northeast, or northwest.",
-                None, None, 22, 20, None, None, None, None, None, 16)  # 21
+    # Room 21
+    room = Room("You are standing on the east side of the field.\n"
+                "You can continue by going south, west, or southwest.",
+                None, None, 22, 20, None, None, None, None, None, 16)
     room_list.append(room)
 
-    room = Room("22 You have entered deeper into the corn field.\n"
-                "You can go deeper into the field by going north, northeast, or northwest.",
-                21, None, None, None, None, None, None, 16, None, 23)  # 22
+    # Room 22
+    room = Room("You are standing on the east side of the field.\n"
+                "You can continue by going north, northwest, or southwest.",
+                21, None, None, None, None, None, None, 16, None, 23)
     room_list.append(room)
 
-    room = Room("23 You have entered deeper into the corn field.\n"
-                "You can go deeper into the field by going north, northeast, or northwest.",
-                16, None, None, None, None, None, 22, None, None, None)  # 23
+    # Room 23
+    room = Room("You are standing along the front fence of the field that prevents you from going south.\n"
+                "You can just make out the top of the house to the southwest.\n"
+                "You can continue by going north, west, or northeast.",
+                16, None, None, 13, None, None, 22, None, None, None)
     room_list.append(room)
 
-    room = Room("24 You have entered deeper into the corn field.\n"
-                "You can go deeper into the field by going north, northeast, or northwest.",
-                18, None, None, None, None, None, 14, None, None, None)  # 24
+    # Room 24
+    room = Room("You are standing along the front fence of the field that prevents you from going south.\n"
+                "You can barley see the top of the barn over the corn in the distant east.\n"
+                "You can continue by going north, east, or northeast.",
+                18, 13, None, None, None, None, 14, None, None, None)
     room_list.append(room)
 
-    room = Room("25 You have entered deeper into the corn field.\n"
-                "You can go deeper into the field by going north, northeast, or northwest.",
-                None, 19, 17, 18, None, None, None, None, 15, None)  # 25
+    # Room 25
+    room = Room("You are standing along the fence line at the opposite end of the property.\n"
+                "You can continue by going east, south, west, or southeast.",
+                None, 19, 17, 18, None, None, None, None, 15, None)
     room_list.append(room)
 
     # Items
@@ -211,6 +243,8 @@ def main():
 
     item = Item("Hay twine is here.", "twine", 11)
     item_list.append(item)
+
+    item = Item("An old key is here.", "key", 6)
 
     current_room = 0
     done = False
